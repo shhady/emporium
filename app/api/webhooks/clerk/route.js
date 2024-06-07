77d6991fa1b1c9fac9c8e1294166775981456b0a
shhady <shhadyse@gmail.com>
2024-06-07 17:20:23 +0300

    second

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -67,7 +67,7 @@ export async function POST(req) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: email_addresses[0].email_address.split("@")[0],
+      username: username ? username : email_addresses[0].email_address.split("@")[0],
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
